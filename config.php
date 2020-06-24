6e4e8bc7126c1fe66c87d6b7ae97d401f66fe6c2
Zachary-T-Labib <67299732+Zachary-T-Labib@users.noreply.github.com>
2020-06-23 20:23:18 -0400

    Created config.php

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+/////
+// config.php
+//
+// This file is not version controlled.
+/////
+
+
+define("ENVIRONMENT", "production");
+define("SERVER_URL", "http://zachsprogram.org");
+define('PROJ_ROOT', '/zachsprogramdotorg');
+
+
+/**
+ * Database Constants
+ */
+define("DB_SERVER", "localhost");
+define("DB_USER", "zachsprojectdotorg");
+define("DB_PASS", "");
+define("DB_NAME", "zachsprojectdotorg");
