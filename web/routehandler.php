83fb997b8188e60a52d13d99e10c997a471ac742
Zachary-T-Labib <dadsfriend08@gmail.com>
2020-07-12 11:53:39 -0400

    Added code to routehandler.php

---
+++ web/routehandler.php
@@ -0,0 +1,20 @@
+<?php
+
+require(__DIR__ . '/../config.php');
+
+define('DIRSEP', DIRECTORY_SEPARATOR);
+define('WEB_DIR', PROJ_ROOT . DIRSEP . 'web');
+define('VENDOR_DIR', PROJ_ROOT . DIRSEP . 'vendor');
+
+
+define('VIEWS', PROJ_ROOT . DIRSEP . 'app' . DIRSEP . 'zachsprogramdotorg' . DIRSEP . 'Views');
+define('VIEWSINCLUDES', PROJ_ROOT . DIRSEP . 'app' . DIRSEP . 'zachsprogramdotorg' . DIRSEP . 'ViewsIncludes');
+define('TOPMOST', VIEWSINCLUDES . DIRSEP . 'topmost.php');
+define('BOTTOMMOST', VIEWSINCLUDES . DIRSEP . 'bottommost.php');
+define('BOTTOM', VIEWSINCLUDES . DIRSEP . 'bottom.php');
+define('SESSIONMESSAGE', VIEWSINCLUDES . DIRSEP . 'sessionmessage.php');
+
+$path3 = VENDOR_DIR . DIRSEP . 'autoload.php';
+$path4 = LIB_PATH . DIRSEP . 'functions.php';
+require $path3;
+require $path4;
