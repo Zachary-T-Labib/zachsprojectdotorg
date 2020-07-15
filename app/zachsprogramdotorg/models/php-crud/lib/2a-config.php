67035f04498364ddd151d730c3682455501bd375
Zachary-T-Labib <dadsfriend08@gmail.com>
2020-07-15 18:01:00 -0400

    Added crud files that run a task book

---
+++ app/zachsprogramdotorg/models/php-crud/lib/2a-config.php
@@ -0,0 +1,14 @@
+<?php
+// SILENT NOTICES
+error_reporting(E_ALL & ~E_NOTICE);
+
+// DATABASE SETTINGS
+// ! CHANGE TO YOUR OWN !
+define("DB_SERVER", "localhost");
+define("DB_USER", "zachsprojectdotorg");
+define("DB_PASS", "kdbgiwmmTB535]");
+define("DB_NAME", "zachsprojectdotorg");
+
+// AUTO PATH
+define("PATH_LIB", __DIR__ . DIRECTORY_SEPARATOR);
+?>
---
+++ app/zachsprogramdotorg/models/php-crud/sql/1-address.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `task_book` (
+`id` int(11) NOT NULL,
+`task` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `task_book`
+ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `task_book`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+INSERT INTO `task_book` (`id`, `address`) VALUES
+(1, 'Work on web application');
