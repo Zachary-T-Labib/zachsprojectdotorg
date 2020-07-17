09306db54d85144fa7b1b70ac48dfc271d75f0b5
Zachary-T-Labib <dadsfriend08@gmail.com>
2020-07-17 13:18:03 -0400

    Changed project to program

--- web/php-crud/lib/2a-config.php
+++ web/php-crud/lib/2a-config.php
@@ -5,9 +5,9 @@ error_reporting(E_ALL & ~E_NOTICE);
 // DATABASE SETTINGS
 // ! CHANGE TO YOUR OWN !
 define("MYSQL_HOST", "localhost");
-define("MYSQL_NAME", "zachsprojectdotorg");
+define("MYSQL_NAME", "zachsprogramdotorg");
 define("MYSQL_CHAR", "utf8");
-define("MYSQL_USER", "zachsprojectdotorg");
+define("MYSQL_USER", "zachsprogramdotorg");
 define("MYSQL_PASS", "kdbgiwmmTB535]");
 
 // AUTO PATH
