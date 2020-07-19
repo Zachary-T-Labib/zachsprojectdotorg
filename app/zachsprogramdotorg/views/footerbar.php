c465ca0180cf25a52567b1dabd262ac4fbb3d6e6
Zachary-T-Labib <dadsfriend08@gmail.com>
2020-07-19 12:28:47 -0400

    Added stylesheet

--- app/zachsprogramdotorg/views/footerbar.php
+++ app/zachsprogramdotorg/views/footerbar.php
@@ -1,22 +1,11 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<style>
-		
-.footerbar {
-    max-width: 500px;
-    margin: auto;
-    background: black;
-}
-
-</style>
-</head>
 
 <body>
 
 <div id="footerbar">
     <p align="center" style="font-size: .78em;">
-		
+		ZL - 2020
 	</p>
       
 	  
