243284fcccd4b23d25d299178da0ad830832997c
Zachary-T-Labib <dadsfriend08@gmail.com>
2020-07-20 16:13:59 -0400

    Added cars class;Extension of ZachObject

---
+++ app/zachsprogramdotorg/models/cars.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace zachsprogramdotorg/models;
+
+class Cars extends ZachObject
+{
+	
+	protected static $table_name = cars
+		
+	protected static $fields = ['id', 'LicensePlate', 'Type', 'Brand', 'Seats']
+		
+	public $id;
+	
+	public $LicensePlate;
+	
+	public $Type;
+	
+	public $Brand;
+	
+	public $Seats;
+}
+
+?>
