5bfe50f4f129fff4a6f96ac43e55bef3a8abc853
Zachary-T-Labib <dadsfriend08@gmail.com>
2020-07-21 12:22:55 -0400

    Moved DIRSEP constant

--- web/cars_table_transfer.php
+++ web/cars_table_transfer.php
@@ -1,8 +1,8 @@
 <?php
 
+define('DIRSEP', DIRECTORY_SEPARATOR);
 define('PROJ_ROOT', '/zachsprogramdotorg');
 define('VIEWS', PROJ_ROOT . DIRSEP . 'app' . DIRSEP . 'zachsprogramdotorg' . DIRSEP . 'views');
-define('DIRSEP', DIRECTORY_SEPARATOR);
 
 require VIEWS . DIRSEP . 'car_table.html'
 	
