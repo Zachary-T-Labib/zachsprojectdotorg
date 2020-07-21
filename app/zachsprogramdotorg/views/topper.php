13892bc431c76e42fcbccd1f41ffad57c4c165eb
Zachary-T-Labib <dadsfriend08@gmail.com>
2020-07-21 15:21:12 -0400

    Changed path

--- app/zachsprogramdotorg/views/topper.php
+++ app/zachsprogramdotorg/views/topper.php
@@ -5,7 +5,7 @@
 
 <div id="topper">
     <p align="center" style="font-size: .78em;">
-		<h1><a href="/web/routehandler.php" class="button"> 🏠 </a></h1>
+		<h1><a href="/zl" class="button"> 🏠 </a></h1>
 	</p>
       
 	  
