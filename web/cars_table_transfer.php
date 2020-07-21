f39a4b0959f5e01fdec79e804b6495704d34af17
Zachary-T-Labib <dadsfriend08@gmail.com>
2020-07-21 12:15:16 -0400

    Defined Constants

--- web/cars_table_transfer.php
+++ web/cars_table_transfer.php
@@ -1,4 +1,6 @@
 <?php
+define('VIEWS', PROJ_ROOT . DIRSEP . 'app' . DIRSEP . 'zachsprogramdotorg' . DIRSEP . 'views');
+define('DIRSEP', DIRECTORY_SEPARATOR);
 
 require VIEWS . DIRSEP . 'car_table.html'
 	
