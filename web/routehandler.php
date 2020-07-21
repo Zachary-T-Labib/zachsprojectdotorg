4eab7141297bbd03473206a91c252a142975fe73
Zachary-T-Labib <dadsfriend08@gmail.com>
2020-07-21 15:03:08 -0400

    Changed spelling

--- web/routehandler.php
+++ web/routehandler.php
@@ -8,7 +8,7 @@ define('VENDOR_DIR', PROJ_ROOT . DIRSEP . 'vendor');
 
 define('VIEWS', PROJ_ROOT . DIRSEP . 'app' . DIRSEP . 'zachsprogramdotorg' . DIRSEP . 'views');
 define('FOOTERBAR', VIEWS . DIRSEP . 'footerbar.php');
-define('TOPPER', VIEWS . DIRSEP . 'toppper.php');
+define('TOPPER', VIEWS . DIRSEP . 'topper.php');
 
 $path3 = VENDOR_DIR . DIRSEP . 'autoload.php';
 $path4 = WEB_DIR . DIRSEP . 'functions.php';
