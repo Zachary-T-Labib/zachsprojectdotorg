b24391c10376f046870eaec3e5406689904fca3c
Zachary-T-Labib <dadsfriend08@gmail.com>
2020-07-21 15:11:05 -0400

    Changed topper; Changed Route

--- app/zachsprogramdotorg/views/topper.php
+++ app/zachsprogramdotorg/views/topper.php
@@ -5,7 +5,7 @@
 
 <div id="topper">
     <p align="center" style="font-size: .78em;">
-		<center><h1><a href="/controllers/Home.php" class="button">-->🏠</a></h1></center>
+		<h1><a href="/web/routehandler.php" class="button"> 🏠 </a></h1>
 	</p>
       
 	  
