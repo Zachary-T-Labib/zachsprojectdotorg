cd661d713413cb37442a91c5770677d19869f864
Zachary-T-Labib <dadsfriend08@gmail.com>
2020-07-23 16:17:29 -0400

    Fixed transfer error

--- web/cars_table_transfer.php
+++ web/cars_table_transfer.php
@@ -4,7 +4,7 @@ define('DIRSEP', DIRECTORY_SEPARATOR);
 define('PROJ_ROOT', '/zachsprogramdotorg');
 define('VIEWS', PROJ_ROOT . DIRSEP . 'app' . DIRSEP . 'zachsprogramdotorg' . DIRSEP . 'views');
 
-require VIEWS . DIRSEP . 'car_table.html'
+require VIEWS . DIRSEP . 'car_table.php'
 	
 	?>
 
