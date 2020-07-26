d705a6984aecbe34b1f0e6355a3041fa118ca69b
Zachary-T-Labib <dadsfriend08@gmail.com>
2020-07-26 14:33:06 -0400

    Testing stuff

--- app/zachsprogramdotorg/controllers/OtherClass.php
+++ app/zachsprogramdotorg/controllers/OtherClass.php
@@ -5,10 +5,15 @@ namespace zachsprogramdotorg\controllers;
 use zachsprogramdotorg\models\ZachObject;
 use zachsprogramdotorg\models\CarsObject;
 
+require MODELS . DIRSEP . 'CarsObject.php';
+
 class OtherClass
 {
      function page()
      {
+
          echo "Other Class works!";
      }
 }
+
+?>
