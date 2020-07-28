98cadc245512f506fc4193c630e159bff3d41681
Zachary-T-Labib <dadsfriend08@gmail.com>
2020-07-28 14:07:22 -0400

    Added fuctions for database and array variable

--- app/zachsprogramdotorg/controllers/Cars.php
+++ app/zachsprogramdotorg/controllers/Cars.php
@@ -8,6 +8,14 @@ class Cars
 {
 	function page()
     {
+		$db = get_db();
+		
+		$sql = 'SELECT * FROM `task` WHERE `user_id` = ' . $db->real_escape_string($user_id);
+		$array = CarsObject::find_by_sql($db, $sessionMessage, $sql);
+		
+		if (!$array || !empty($sessionMessage)) {
+		            breakout(' I could NOT find any tasks. ');
+		        }
 		
 		require VIEWS . DIRSEP . 'car_table.php';
 	}
--- web/functions.php
+++ web/functions.php
@@ -14,3 +14,42 @@ function convert_snake_case_to_pascal_case(string $string)
 {
     return str_replace('_', '', ucwords($string, '_'));
 }
+
+
+function get_db()
+{
+    global $sessionMessage;
+
+    $db = db_connect($sessionMessage);
+
+    if (!empty($sessionMessage) || $db === false) {
+        breakout(' I was unable to connect to the database. ');
+    }
+
+    return $db;
+}
+
+function db_connect(string &$error)
+{
+    try {
+
+        $db = new mysqli(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
+
+        if ($db->connect_error) {
+
+            $error .= ' ' . htmlspecialchars($db->connect_error, ENT_NOQUOTES | ENT_HTML5) . ' ';
+            return false;
+
+        }
+
+        $db->set_charset('utf8mb4');
+
+    } catch (\Exception $e) {
+
+        $error .= ' ' . htmlspecialchars($e->getMessage(), ENT_NOQUOTES | ENT_HTML5) . ' ';
+        return false;
+
+    }
+
+    return $db;
+}
