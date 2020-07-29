38e144d47f9ed5bad79580227172da5e9c54f0dd
Zachary-T-Labib <dadsfriend08@gmail.com>
2020-07-29 11:48:23 -0400

    Added some details

--- app/zachsprogramdotorg/views/car_table.php
+++ app/zachsprogramdotorg/views/car_table.php
@@ -27,7 +27,7 @@
 		<p></p>
         <?php if (!empty($array)): ?>
             <?php foreach ($array as $key => $object): ?>
-                <p>⇀ <em><?= $object->LicensePlate ?></em> ◜ <?= $object->Type ?> ⇁ <?= $object->Brand ?> ◜ <?= $object->Brand ?></p>
+                <p>⇀ <em>License Plate: <?= $object->LicensePlate ?></em> ◜ Type: <?= $object->Type ?> ⇁ Brand: <?= $object->Brand ?> ◜ Seats: <?= $object->Seats ?></p>
             <?php endforeach; ?>
         <?php else: ?>
             <p>There are no cars.</p>
