f2a220b7973434241ae010fbe48ee5a3868d17a2
Zachary-T-Labib <dadsfriend08@gmail.com>
2020-07-30 13:26:44 -0400

    Added createcar view

---
+++ app/zachsprogramdotorg/views/createcar.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="/css/styles.css">
+</head>
+<body>
+	<?php require TOPPER; ?>
+	<form action="/zl/CreateCar/page" method="post">
+		<h1>Create a Task</h1>
+	    <section>
+	        <p>
+	            <label for="LicensePlate">License Plate: </label>
+	            <input id="LicensePlate" name="LicensePlate" type="text" value="" required minlength="3" maxlength="8"
+	                   size="61" spellcheck="false">
+	        </p>
+	        <p>
+	            <label for="Type">Type: </label>
+	            <input id="Type" name="Type" type="text" value="" required minlength="1" maxlength="35"
+	                   size="61" spellcheck="false">
+	        </p>
+	        <p>
+	            <label for="Brand">Brand: </label>
+	            <input id="Brand" name="Brand" type="text" value="" required minlength="1" maxlength="30"
+	                   size="61" spellcheck="false">
+	        </p>
+			<p>
+	            <label for="Seats">Seats: </label>
+	            <input id="Seats" name="Seats" type="text" value="" required minlength="1" maxlength="16"
+	                   size="61" spellcheck="false">
+		    </p>
+	    </section>
+	        <?php require SUBMITEXIT; ?>
+	</form>
+	<?php require FOOTERBAR; ?>
+	
+	</html>
