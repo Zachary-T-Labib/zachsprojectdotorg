12ba58adf0e45b275434c82272023c796c181e90
Zachary-T-Labib <dadsfriend08@gmail.com>
2020-07-31 17:44:13 -0400

    Fixed Error

--- app/zachsprogramdotorg/controllerincludes/get_car.php
+++ app/zachsprogramdotorg/controllerincludes/get_car.php
@@ -17,7 +17,7 @@ $_SESSION['saved_int01'] = $chosen_id;
 
 $db = get_db();
 
-$object = CarObject::find_by_id($db, $sessionMessage, $chosen_id);
+$object = CarsObject::find_by_id($db, $sessionMessage, $chosen_id);
 
 if (!$object) {
 
