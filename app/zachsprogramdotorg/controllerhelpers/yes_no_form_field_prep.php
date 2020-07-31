94b7fa850ad63c63dd5a0678ec587ff9cec8b3d9
Zachary-T-Labib <dadsfriend08@gmail.com>
2020-07-31 17:38:35 -0400

    Completly added yes_no_form_field_prep

--- app/zachsprogramdotorg/controllerhelpers/yes_no_form_field_prep.php
+++ app/zachsprogramdotorg/controllerhelpers/yes_no_form_field_prep.php
@@ -1,1 +1,19 @@
-.
+<?php
+
+namespace zachsprogramdotorg\controllerhelpers;
+
+function yes_no_form_field_prep(string $field_name): string
+{
+	
+	require_once CONTROLLERHELPERS . DIRSEP . 'standard_form_field_prep.php';
+	
+	$choice = standard_form_field_prep($field_name, 2, 3);
+	
+    if ($choice != "yes" && $choice != "no") {
+
+        breakout(' You didn\'t enter a yes/no choice. ');
+
+    }
+	
+	return $choice;
+}
