872b598881da8fd068d078795b3852ebf0dce911
Zachary-T-Labib <dadsfriend08@gmail.com>
2020-08-02 13:17:39 -0400

    Added UpdateCarEdit controller

---
+++ app/zachsprogramdotorg/controllers/UpdateCarEdit.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace zachsprogramdotorg\controllers;
+
+class UpdateCarEdit
+{
+	function page()
+	{
+		
+		require CONTROLLERINCLUDES . DIRSEP . 'get_car.php';
+	
+		require VIEWS . DIRSEP . 'updatecaredit.php';
+		
+	}
+	
+}
