d0c355b74e8167d66ce88b5d4853a09142d86e91
Zachary-T-Labib <dadsfriend08@gmail.com>
2020-08-07 13:24:34 -0400

    Added fonts to the rest of the views

--- app/zachsprogramdotorg/views/createcar.php
+++ app/zachsprogramdotorg/views/createcar.php
@@ -1,8 +1,19 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<link href="https://fonts.googleapis.com/css2?family=Quantico&family=Ubuntu:wght@500&display=swap" rel="stylesheet">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="/css/styles.css">
+	
+	<style>	
+		h1 {
+		font-family: 'Ubuntu', sans-serif;
+	    }
+		p {
+		font-family: 'Quantico', sans-serif;
+	    }
+	</style>
+	
 </head>
 <body>
 	<div class="mainholder">
