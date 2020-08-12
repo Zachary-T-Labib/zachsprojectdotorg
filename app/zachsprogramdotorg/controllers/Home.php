c3e6781d63c9551e82d2e7c9d96aa6eb09624c18
Zachary-T-Labib <dadsfriend08@gmail.com>
2020-08-12 15:50:13 -0400

    Added redirct logged out users function

--- app/zachsprogramdotorg/controllers/Home.php
+++ app/zachsprogramdotorg/controllers/Home.php
@@ -6,6 +6,19 @@ class Home
 {
     function page()
     {
+        global $sessionMessage;
+        global $is_logged_in;
+		
+		self::redirect_if_not_logged_in($sessionMessage, $is_logged_in);
+		
+		private static function redirect_if_not_logged_in($error, bool $is_logged_in)
+		    {
+		        if (!$is_logged_in) {
+		            $_SESSION['message'] = $error;
+		            reset_feature_session_vars();
+		            redirect_to("/zl/LoginForm/page");
+		        }
+		
 		require '/zachsprogramdotorg/app/zachsprogramdotorg/views/display.html';
     }
 }
