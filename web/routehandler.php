2acb5cc893736a73dac1604917f8d80f8909c045
Zachary-T-Labib <dadsfriend08@gmail.com>
2020-08-14 16:06:12 -0400

    Added viewsincludes folder and moved files into it

--- web/routehandler.php
+++ web/routehandler.php
@@ -7,13 +7,14 @@ define('WEB_DIR', PROJ_ROOT . DIRSEP . 'web');
 define('VENDOR_DIR', PROJ_ROOT . DIRSEP . 'vendor');
 
 define('VIEWS', PROJ_ROOT . DIRSEP . 'app' . DIRSEP . 'zachsprogramdotorg' . DIRSEP . 'views');
+define('VIEWSINCLUDES', PROJ_ROOT . DIRSEP . 'app' . DIRSEP . 'zachsprogramdotorg' . DIRSEP . 'viewsincludes');
 define('CONTROLLERHELPERS', PROJ_ROOT . DIRSEP . 'app' . DIRSEP . 'zachsprogramdotorg' . DIRSEP . 'controllerhelpers');
 define('CONTROLLERINCLUDES', PROJ_ROOT . DIRSEP . 'app' . DIRSEP . 'zachsprogramdotorg' . DIRSEP . 'controllerincludes');
 
 define('SESSIONMESSAGE', VIEWSINCLUDES . DIRSEP . 'sessionmessage.php');
-define('FOOTERBAR', VIEWS . DIRSEP . 'footerbar.php');
-define('TOPPER', VIEWS . DIRSEP . 'topper.php');
-define('SUBMITEXIT', VIEWS . DIRSEP . 'submitexit.php');
+define('FOOTERBAR', VIEWSINCLUDES . DIRSEP . 'footerbar.php');
+define('TOPPER', VIEWSINCLUDES . DIRSEP . 'topper.php');
+define('SUBMITEXIT', VIEWSINCLUDES . DIRSEP . 'submitexit.php');
 
 $path3 = VENDOR_DIR . DIRSEP . 'autoload.php';
 $path4 = WEB_DIR . DIRSEP . 'functions.php';
