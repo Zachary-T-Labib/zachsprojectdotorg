68e73f6a49e793f26349b23a0dc9a5ca5df80b2a
Zachary-T-Labib <dadsfriend08@gmail.com>
2020-08-16 15:50:55 -0400

    Changed format

--- app/zachsprogramdotorg/viewsincludes/topper.php
+++ app/zachsprogramdotorg/viewsincludes/topper.php
@@ -6,9 +6,9 @@
 <div id="topper">
     <p align="center" style="font-size: .78em;">
 
-		<h1><a href="/zl" class="button">:  🏠 </a></h1>
-        <h1>.                  .</h1>
-		<h1><a href="/zl/Logout/page">⭕ Log out! </a></h1>
+		<h1><a href="/zl" class="button">:  🏠 </a>
+        .               .
+		<a href="/zl/Logout/page">⭕ Log out! </a></h1>
 		
 	</p>
       
