d1d7a882baa1aee627d4cd6fe730dbed9329312a
Zachary-T-Labib <dadsfriend08@gmail.com>
2020-08-20 16:47:55 -0400

    Hopefully fixed greeter

--- app/zachsprogramdotorg/viewsincludes/topper.php
+++ app/zachsprogramdotorg/viewsincludes/topper.php
@@ -18,7 +18,7 @@
 <div id="greeter">
 	<p align="center" style="font-size: .78em;">
 		
-		<h3>Welcome, <?php echo $username; ?></h3>
+		<h3>Welcome, <?php echo $_SESSION['user_username']; ?></h3>
 		
 	</p>
 </div>
