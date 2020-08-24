582f436dc1db5e13110be809be7658ec3a45e168
Zachary-T-Labib <dadsfriend08@gmail.com>
2020-08-24 15:15:47 -0400

    Aligned the two divs

--- app/zachsprogramdotorg/viewsincludes/topper.php
+++ app/zachsprogramdotorg/viewsincludes/topper.php
@@ -15,7 +15,7 @@
 	</p>
 </div>
 
-<p>
+<div>
 <div id="greeter">
 		
 		<center><h3 align="center" style="font-size: .78em;" >Welcome, <?php echo $_SESSION['user_username']; ?> 👔</h3></center>
@@ -25,7 +25,8 @@
 <div id="admindiv">
 	<a href="/zl/AdminTools/page"><img src="/img/adminbox.png" alt="Admin Panel" height="86" width="108"></a>
 </div>
-</p>
+</div>
+
 </body>
 </html>
 
