41c4e11e6ec2fad6ba04f5d815f4430187fad8ed
Zachary-T-Labib <dadsfriend08@gmail.com>
2020-09-03 12:23:40 -0400

    Added username_for_specifying_which_prep

---
+++ app/zachsprogramdotorg/controllerhelpers/username_for_specifying_which_prep.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace zachsprogramdotorg\controlerhelpers;
+
+use mysqli;
+
+/**
+ * @param mysqli $db
+ * @return string
+ */
+function username_for_specifying_which_prep(mysqli $db): string
+{
+    /**
+     * Get the submitted username. It is assumed that this username is being submitted solely to identify an account.
+     *
+     * So what are we checking it for?
+     * 1) The standard text field with string length limits.
+     * 2) If it fits the requirements for what a GTK.io username should look like.
+     * 3) If it represents a user account.
+     */
+
+    global $sessionMessage;
+
+    require_once CONTROLLERHELPERS . DIRSEP . 'standard_form_field_prep.php';
+
+    $submitted_username = standard_form_field_prep('username', 7, 12);
+	
+    require_once CONTROLLERHELPERS . DIRSEP . 'is_username_syntactandexists.php';
+
+    if (!is_username_syntactandexists($db, $sessionMessage, $submitted_username)) {
+
+        breakout(' The username field failed validation. ');
+    }
+	
+	return $submitted_username;
+}
