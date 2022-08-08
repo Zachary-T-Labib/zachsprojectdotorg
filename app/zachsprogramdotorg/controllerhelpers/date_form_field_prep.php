3ed37bd369ddfac307c6ac9449e79334db94f071
Zachary-T-Labib <dadsfriend08@gmail.com>
2022-08-08 11:52:18 -0400

    Added date_form_field_prep.php and its required files

---
+++ app/zachsprogramdotorg/controllerhelpers/date_form_field_prep.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace zachsprogramdotorg\controllerhelpers;
+
+/**
+ * @param string $field_name
+ * @return string
+ */
+function date_form_field_prep(string $field_name): string
+{
+    require_once CONTROLLERHELPERS . DIRSEP . 'standard_form_field_prep.php';
+
+    $submitted_date = standard_form_field_prep($field_name, 10, 14);
+
+    require_once CONTROLLERHELPERS . DIRSEP . 'is_date.php';
+
+    if (!is_date($submitted_date)) {
+
+        breakout(' This date value is invalid. ');
+
+    }
+
+    return $submitted_date;
+}
---
+++ app/zachsprogramdotorg/controllerhelpers/is_date.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace zachsprogramdotorg\controllerhelpers;
+
+/**
+ * @param string $date
+ * @return bool
+ */
+function is_date(string &$date): bool
+{
+    /**
+     * Returns true if $date string is formatted as mm/dd/yyyy and is a real calendar date.
+     * Otherwise, returns false.
+     *
+     * Side Effects: - $date will be trimmed.
+     *               - $message may be modified.
+     */
+
+    global $g;
+
+    $date = trim($date);
+
+    if (empty($date)) {
+
+        $g->message .= " The date is missing. ";
+
+        return false;
+
+    }
+
+    $number_of_slashes = substr_count($date, '/');
+
+    if ($number_of_slashes != 2) {
+
+        $g->message .= " You don't have two slashes in date. ";
+
+        return false;
+    }
+
+
+    /**
+     * Split date into its parts.
+     */
+
+    $words = explode('/', $date);
+
+    $mm = $words[0];
+
+    $dd = $words[1];
+
+    $yyyy = $words[2];
+
+    if (strlen($mm) != 2 || strlen($dd) != 2 || strlen($yyyy) != 4) {
+
+        $g->message .= " You did not use correct mm/dd/yyyy date format. ";
+
+        return false;
+    }
+
+    if (!is_numeric($mm) || !is_numeric($dd) || !is_numeric($yyyy)) {
+
+        $g->message .= " The date must consist of numeric digits and 2 forward slashes. And, it does not have
+            required numeric digits! ";
+
+        return false;
+    }
+
+    if (!checkdate($words[0], $words[1], $words[2])) {
+
+        $g->message .= " That's not a valid date. ";
+
+        return false;
+    }
+
+    return true;
+}
