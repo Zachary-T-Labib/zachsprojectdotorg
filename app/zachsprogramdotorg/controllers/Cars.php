237c4d23a7bd66abba19cd9cf5cebc90cf20ce0e
Zachary-T-Labib <dadsfriend08@gmail.com>
2022-08-09 18:12:30 -0400

    Removed some arguements

--- app/zachsprogramdotorg/controllers/Cars.php
+++ app/zachsprogramdotorg/controllers/Cars.php
@@ -7,13 +7,11 @@ use zachsprogramdotorg\models\CarsObject;
 class Cars
 {
 	function page()
-    {
-		global $sessionMessage;
-		
-		$db = get_db();
+    {	
+		get_db();
 		
 		$sql = 'SELECT * FROM `carsobject`';
-		$array = CarsObject::find_by_sql($db, $sessionMessage, $sql);
+		$array = CarsObject::find_by_sql($sql);
 		
 		
 		require VIEWS . DIRSEP . 'car_table.php';
