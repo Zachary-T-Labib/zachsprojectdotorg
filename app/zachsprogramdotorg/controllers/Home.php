05e0c6883b5ff8dfca818244b3d4ed49c57ffc44
Zachary-T-Labib <dadsfriend08@gmail.com>
2022-08-09 18:02:45 -0400

    Added  functionality to Home

--- app/zachsprogramdotorg/controllers/Home.php
+++ app/zachsprogramdotorg/controllers/Home.php
@@ -2,25 +2,28 @@
 
 namespace zachsprogramdotorg\controllers;
 
+use zachsprogramdotorg\models\User;
+
 class Home
 {
     function page()
     {
-        global $sessionMessage;
-        global $is_logged_in;
-		global $is_admin;
 		
-		self::redirect_if_not_logged_in($sessionMessage, $is_logged_in);
+		self::redirect_if_not_logged_in();
 		
 		require '/zachsprogramdotorg/app/zachsprogramdotorg/views/display.html';
     }
 	
-    private static function redirect_if_not_logged_in($error, bool $is_logged_in)
+    private static function redirect_if_not_logged_in()
        {
-           if (!$is_logged_in) {
-               $_SESSION['message'] = $error;
-               reset_feature_session_vars();
-               redirect_to("/zl/LoginForm/page");
-           }
-	   }
+		   global $g
+		   
+		   if (!$g->is_logged_in) {
+
+		               $_SESSION['message'] = $g->message;
+		               reset_feature_session_vars();
+		               redirect_to("/zl/LoginForm/page");
+
+		           }
+	 }
 }
